82059d4f65b74855d76e3ef927f5879aed7eaf15
BN2021 <botova.ekaterina00mail.ru>
2021-04-26 21:12:34 +0500

    Новое изменение файла testcmp.cpp

--- testcmp.cpp
+++ testcmp.cpp
@@ -14,14 +14,17 @@ int main()
    cout << " A = B + C, A =" << A << endl;
 
    Complex M = B - C;
+
    cout << " M = B - C, M = " << M << endl;
    cout << " M * A = " << M * A << endl;
    cout << " M / 4.45 = " << M / 4.45 << endl;
 
-    Complex D;
-  	cout << " Enter complex number D = ";
-    cin >> D;
-    A + = C + D;
-    cout << " D = " << D << " \nA = " << A << endl;
-    return 0;
+   Complex D;
+
+   cout << " Enter complex number D = ";
+   cin >> D;
+   A + = C + D;
+   cout << " D = " << D << " \nA = " << A << endl;
+
+   return 0;
 }
